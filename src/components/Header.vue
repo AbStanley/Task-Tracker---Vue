2251cd979686c149ab64ec01939aaa03eeb56b1f
AbStanley <abrahamx.carranza@gmail.com>
2021-05-25 17:23:05 -0600

    Project (in progress)

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <div>
+            <h1>Task Tracker</h1>
+            <h2>{{ title }}</h2>
+        </div>
+        <Button text="Add Task" color="Green"/>
+        <Button text="Update task" color="Orange"/>
+        <Button text="Remove task" color="Red"/>
+        
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    components: {
+        Button,
+    },
+    props: {
+        title: { 
+            type: String,
+            default: 'Guess',
+        },
+    },
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+        }
+
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :key="task.id" v-for="task in tasks" >
+        <!-- Loop trough the array 
+        annd show what the task comp says.
+        -->
+        <Task :task="task" />
+    </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    },
+    components: {
+       Task,  
+    }
+}
+</script>
